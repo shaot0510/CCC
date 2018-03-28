5db34db776461a780c4f9b7c1fca0f7885feef9b
shaot0510 <37129942+shaot0510@users.noreply.github.com>
2018-03-28 14:38:03 -0400

    Add files via upload

---
+++ first program.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  first program
+//
+//  Created by Owner on 3/6/18.
+//  Copyright © 2018 Owner. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    a=1;
+    b=2;
+    printf("%d\n", a+b);
+    return 0;
+}
